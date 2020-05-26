6f601b54b622e2a4450739552cb01e0a1576f595
HwanHo Lee <leehh1412@hotmail.com>
2020-05-26 14:35:03 +0900

    초기 커밋.

---
+++ ClosedXMLSample/ExcelExport.cs
@@ -0,0 +1,38 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClosedXMLSample
+{
+    public class ExcelExport
+    {
+        public const string TempA = "A";
+        public const string TempB = "B";
+        public const string TempC = "C";
+        public const string Humidity = "D";
+
+        public static void Export(List<SensorData> sensorDatas)
+        {
+            int count = 2;
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Sample Sheet");
+                worksheet.Cell($"{TempA}1").Value = "온도A";
+                worksheet.Cell($"{TempB}1").Value = "온도B";
+                worksheet.Cell($"{TempC}1").Value = "온도C";
+                worksheet.Cell($"{Humidity}1").Value = "습도";
+                foreach (var item in sensorDatas)
+                {
+                    worksheet.Cell($"{TempA}{count}").Value = item.TempA.ToString();
+                    worksheet.Cell($"{TempB}{count}").Value = item.TempB.ToString();
+                    worksheet.Cell($"{TempC}{count}").Value = item.TempC.ToString();
+                    worksheet.Cell($"{Humidity}{count}").Value = item.Humidity.ToString();
+                    count++;
+                }
+                workbook.SaveAs("HelloWorld.xlsx");
+            }
+
+        }
+    }
+}
---
+++ ClosedXMLSample/SensorData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClosedXMLSample
+{
+    public class SensorData
+    {
+        public int TempA { get; set; }
+        public int TempB { get; set; }
+        public int TempC { get; set; }
+        public int Humidity { get; set; }
+
+        public readonly static Random rnd = new Random();
+        public static SensorData CreateSensorData()
+        {
+            SensorData sd = new SensorData();
+            sd.TempA = rnd.Next(1, 500);
+            sd.TempB = rnd.Next(1, 500);
+            sd.TempC = rnd.Next(1, 500);
+            sd.Humidity = rnd.Next(1, 500);
+            return sd;
+        }
+    }
+}
